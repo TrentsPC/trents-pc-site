58954d24a459959d54b689cb087b7e2f998f020d
Trent Mitchell-Borley <trent@trents.computer>
2023-01-26 23:48:39 +1300

    use vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { macaronVitePlugin } from "@macaron-css/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [macaronVitePlugin(), solid()],
+  plugins: [
+    macaronVitePlugin(),
+    solid({
+      adapter: vercel(),
+    }),
+  ],
 });
